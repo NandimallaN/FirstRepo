426fa0f89ee1223642c2c4c4df59f31b2167332a
Nageswara <nageswara.nandimalla@gmail.com>
2024-01-08 12:53:45 -0500

    Day4 commit two

---
+++ Day-4-ClassesandObjects/src/com/app/inh/Manager.java
@@ -0,0 +1,19 @@
+package com.app.inh;
+
+public class Manager extends Employee{
+   private String desig;
+
+public Manager(String desig) {
+	super(101,"Ramana",10000);
+	this.desig = desig;
+}
+
+@Override
+public String toString() {
+	return "Manager [desig=" + desig + ", toString()=" + super.toString() + "]";
+}
+	
+
+
+   
+}
