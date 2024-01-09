74fba9960e103d048fa9170c7e3f22d54101bc63
Nageswara <nageswara.nandimalla@gmail.com>
2024-01-09 12:16:19 -0500

    Day5 Examples One

---
+++ Day5-OOPS-Day2/src/com/app/activity/C.java
@@ -0,0 +1,11 @@
+package com.app.activity;
+
+import com.app.examples.A;
+
+public class C extends A{
+	
+	 public void show()
+	{
+		System.out.println(" show method from Class C"+x);
+	}
+}
---
+++ Day5-OOPS-Day2/src/com/app/examples/A.java
@@ -0,0 +1,10 @@
+package com.app.examples;
+
+public  class A
+{
+	 public  int x=100;
+	 public void show()
+	{
+		System.out.println(" show method from Class A"+x);
+	}
+}
