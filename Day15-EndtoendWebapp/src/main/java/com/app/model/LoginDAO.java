4bc59cc506576416c3c7f64f93045b42a7b53752
Nageswara <nageswara.nandimalla@gmail.com>
2024-01-24 14:58:10 -0500

    end to end project

---
+++ Day15-EndtoendWebapp/src/main/java/com/app/model/LoginDAO.java
@@ -0,0 +1,10 @@
+package com.app.model;
+
+public interface LoginDAO {
+	
+	
+String query="select role from user where username=? and password=?";
+	
+	String getRoleFromDB(String uname,String pswd);
+
+}
